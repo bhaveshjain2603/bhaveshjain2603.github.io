b4403c178a304bd745abcc591563f073f87cd1e9
Bhavesh Jain <126448088+bhaveshjain2603@users.noreply.github.com>
2023-11-05 22:16:59 +0530

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -3,23 +3,37 @@ let buttons = document.querySelectorAll('button');
 let string = "";
 let array = Array.from(buttons);
 
-array.forEach(button =>{
+array.forEach(button => {
     button.addEventListener('click', (e) => {
-        if(e.target.innerHTML == '='){
-            string = eval(string);
-            input.value = string;
-        }
-        else if(e.target.innerHTML == 'AC'){
-            string = "";
-            input.value = string;
-        }
-        else if(e.target.innerHTML == 'DEL'){
-            string = string.substring(0, string.length-1);
-            input.value = string;
-        }
-        else{
-            string += e.target.innerHTML;
-            input.value = string;
-        }
-    })
-})
+        handleButtonClick(e.target.innerHTML);
+    });
+});
+
+document.addEventListener('keydown', (e) => {
+    const key = e.key;
+    if (key === 'Enter') {
+        handleButtonClick('=');
+    } else if (key === 'Backspace') {
+        handleButtonClick('DEL');
+    } else if (key === 'Escape') {
+        handleButtonClick('AC');
+    } else if (/[0-9+\-*/.%]/.test(key)) {
+        handleButtonClick(key);
+    }
+});
+
+function handleButtonClick(value) {
+    if (value === '=') {
+        string = eval(string);
+        input.value = string;
+    } else if (value === 'AC') {
+        string = "";
+        input.value = string;
+    } else if (value === 'DEL') {
+        string = string.substring(0, string.length - 1);
+        input.value = string;
+    } else {
+        string += value;
+        input.value = string;
+    }
+}
